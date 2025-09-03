aacd3eb9f4ade0987b0fcfb2cbc71a1f1a23e364
AFoGC <sirkoandrew84@gmail.com>
2025-09-03 23:29:53 +0300

    Исправил форматирование оценок

---
+++ Filmc.Wpf/Converters/MarkValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Filmc.Wpf.Converters
+{
+    public class MarkValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values[0] == null)
+                return String.Empty;
+
+            return $"{values[0]}/{values[1]}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // Not used for one-way binding
+            throw new NotImplementedException();
+        }
+    }
+}
